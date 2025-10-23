a42d62aae9bbc3d2593aa767d15ea27f2947de09
avilu <lubicka@ramaz.org>
2025-10-23 12:32:09 -0400

    :D

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import os
 from typing import Union, Annotated
-from fastapi import FastAPI, Form, Depends
 import pymongo
 from pymongo.server_api import ServerApi
 from pymongo.mongo_client import MongoClient
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
-
-
+from fastapi import Depends, FastAPI, HTTPException, status, Form
+from pwdlib import PasswordHash
 import os
-
-app = FastAPI()
-
 from dotenv import load_dotenv
 
+
+
+app = FastAPI()
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+
 load_dotenv()  # take environment variables
-
-
-
-
 
 uri = os.environ["MONGODB_URL"]
 # Create a new client and connect to the server
@@ -29,6 +32,24 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
 
 class User(BaseModel):
     username: str
@@ -41,19 +62,16 @@ col = db.get_collection("User_Info")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+
+
+
 def fake_hash_password(password: str):
     return "fakehashed" + password
-
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/login")
-def read_root():
-    return {"Hello": "World"}
 
 
 @app.post("/login")
@@ -68,22 +86,60 @@ async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"token": token}
 
 
+
+class UserInDB(User):
+    hashed_password: str
+password_hash = PasswordHash.recommended()
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
 def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-    )
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
 
 
 @app.get("/users/me")
-async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
     return current_user
-
-
 
 
     user = mycol.find_one({"username": username, "password": password})
