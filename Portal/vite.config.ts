45000182b6ca0a7bbe58cc0027f43087946347a1
avilu <lubicka@ramaz.org>
2026-02-10 12:58:06 -0500

    :D

--- Portal/vite.config.ts
+++ Portal/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/portal/", // IMPORTANT so asset URLs work when served under /portal
+});
